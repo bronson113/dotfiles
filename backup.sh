84fecbeae5f93b785c8ad683ed31c591f6f08e6a
Bronson Yen <bronson1013@gmail.com>
2023-01-18 18:39:52 -0500

    Backup_2023-01-18_18:39:52

--- backup.sh
+++ backup.sh
@@ -3,10 +3,10 @@
 # Check to see is git command line installed in this machine
 IS_GIT_AVAILABLE=`git --version`
 if [[ $IS_GIT_AVAILABLE == *"version"* ]]; then
-  echo "Git is Available"
+    echo "git is Available"
 else
-  echo "Git is not installed"
-  exit 1
+    echo "git is not installed"
+    exit 1
 fi
 
 function dotconfig {
@@ -20,8 +20,12 @@ gs="$(dotconfig status | grep -i "modified")"
 
 # If there is a new change
 if [[ $gs == *"modified"* ]]; then
-  echo "push"
+    echo "push"
+else
+    echo "no change detected"
+    exit 1
 fi
+
 
 
 # push to Github
