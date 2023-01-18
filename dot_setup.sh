c75fcb30d899f9c601d3cce8d88da6b34c0d0a1f
Bronson Yen <bronson1013@gmail.com>
2023-01-18 17:11:51 -0500

    add cloning submodules

--- dot_setup.sh
+++ dot_setup.sh
@@ -1,9 +1,15 @@
+#!/bin/bash
+
+# get the repository
 git clone --bare https://github.com/bronson113/dotfiles.git $HOME/.cfg
+
+# run git within the context
 function dotconfig {
     /usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME $@
 }
 mkdir -p .config-backup
-dotconfig checkout
+
+# update the files
 if [ $? = 0 ]; then
     echo "Checked out config.";
 else
@@ -13,6 +19,13 @@ fi;
 dotconfig checkout
 dotconfig config status.showUntrackedFiles no
 
+# clone sub modules (can't automate due to bare repo)
+git clone https://github.com/gpakosz/.tmux.git
+git clone https://github.com/scwuaptx/Pwngdb.git
+git clone https://github.com/bronson113/mktmpdir.git
+
+
+# setup vim
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 vim +PlugUpgrade +PlugUpdate +PlugInstall +q
 
